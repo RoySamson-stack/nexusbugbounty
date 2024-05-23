1a7b8f078213cb4a3069d7098a4006fd72d17708
RoySamson-stack <you@example.com>
2024-05-23 23:05:38 +0300

    creating the forms for the login and signup

--- bugbounty/views.py
+++ bugbounty/views.py
@@ -14,3 +14,29 @@ def signup(request):
         form = UserCreatingForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')        
